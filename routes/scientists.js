0eec375682d6860fa8c36e6c6f253e303dd58c3d
Priyansh Dimri <priyanshd0908@gmail.com>
2025-03-24 13:02:19 +0530

    add items and scientists routes

---
+++ routes/scientists.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const scientistController = require("../controllers/scientistController");
+
+router.get("/", scientistController.getAllScientists);
+
+router.get("/:id", scientistController.getScientistById);
+
+router.post("/", scientistController.createScientist);
+
+router.put("/:id", scientistController.updateScientist);
+
+router.delete("/:id", scientistController.deleteScientist);
+
+module.exports = router;
